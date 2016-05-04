925592766fdfddbe384f865895d8a4e2202e0246
SilverBirchh <Bradley.Birch@ig.com>
2016-05-04 13:39:16 +0100

    Login flow AC's

--- tests/acceptance/login-test.js
+++ tests/acceptance/login-test.js
@@ -1,31 +1,76 @@
 /* jshint expr:true */
 import {
-  describe,
-  it,
-  beforeEach,
-  afterEach
+	describe,
+	it,
+	beforeEach,
+	afterEach
 } from 'mocha';
-import { expect } from 'chai';
+import {
+	expect
+} from 'chai';
 import startApp from '../helpers/start-app';
 import destroyApp from '../helpers/destroy-app';
-import { currentSession, authenticateSession, invalidateSession } from 'soar-trading/tests/helpers/ember-simple-auth';
+import {
+	currentSession,
+	authenticateSession,
+	invalidateSession
+} from '../helpers/ember-simple-auth';
 
-describe('Acceptance: Login', function() {
-  let application;
+describe('Acceptance: Login - The different login flows', function() {
+	let application;
 
-  beforeEach(function() {
-    application = startApp();
-  });
+	beforeEach(function() {
+		application = startApp();
+	});
 
-  afterEach(function() {
-    destroyApp(application);
-  });
+	afterEach(function() {
+		destroyApp(application);
+	});
 
-  it('can login and redirct to trade page', function() {
-    visit('/members/login');
-    
-    andThen(function() {
-      expect(currentPath()).to.equal('members.login');
-    });
-  });
+	it('can login and redirct to trade page', function() {
+		visit('/members/login');
+		authenticateSession(application, {
+			userId: 1,
+			otherData: 'some-data'
+		});
+
+		andThen(function() {
+			expect(currentPath()).to.equal('trade');
+			let session = currentSession(application);
+			expect(session.get('data.authenticated.userId')).to.eql(1);
+			expect(session.get('data.authenticated.otherData')).to.eql('some-data');
+		});
+	});
+
+	it('can login and then not access login page', function() {
+		visit('/members/login');
+		authenticateSession(application, {
+			userId: 1,
+			otherData: 'some-data'
+		});
+		visit('/members/login');
+		andThen(function() {
+			expect(currentPath()).to.equal('index');
+		});
+	});
+
+	it('can login and then not access login page', function() {
+		visit('/members/login');
+		authenticateSession(application, {
+			userId: 1,
+			otherData: 'some-data'
+		});
+		visit('/members/sign-up');
+		andThen(function() {
+			expect(currentPath()).to.equal('index');
+		});
+	});
+
+	it('can not visit trade section when unauthorized', function() {
+		invalidateSession(application);
+		visit('/trade');
+		andThen(function() {
+			expect(currentPath()).to.equal('index');
+		});
+	});
 });
