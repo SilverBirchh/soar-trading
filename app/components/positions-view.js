4bde2267c1386a7910d42bbb832d227d347c7973
Bradley Birch <Bradley.Birch@ig.com>
2016-05-27 15:44:21 +0100

    Stying and form change

--- app/components/positions-view.js
+++ app/components/positions-view.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	actions: {
+		toggleClass(position) {
+			$(`#${position.dealId}`).toggleClass('inline-close');
+			$(`#${position.dealId} form`).toggleClass('hidden');
+		}
+	}
 });
