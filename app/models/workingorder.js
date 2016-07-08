c05123d35f03c939dc31585353ffa68e3ebf52a6
Bradley Birch <Bradley.Birch@ig.com>
2016-07-08 10:39:36 +0100

    WO refactor

--- app/models/workingorder.js
+++ app/models/workingorder.js
@@ -2,15 +2,6 @@ import DS from 'ember-data';
 import Ember from 'ember';
 
 export default DS.Model.extend({
-  pnlService: Ember.inject.service('pnl-service'),
-
   workingOrderData: DS.attr(),
   marketData: DS.attr(),
-  liveData: Ember.computed(function() {
-		const market = this.get('marketData');
-    const order = this.get('workingOrderData');
-		const epic = market.epic;
-    const direction = order.direction;
-		return this.get('pnlService').getLatest(epic, direction);
-	})
 });
--- app/services/pnl-service.js
+++ app/services/pnl-service.js
@@ -11,44 +11,10 @@ export default Ember.Service.extend({
       "MERGE", market, latestDirection
     );
     subscription.setRequestedSnapshot("yes");
-		subscription.addListener({
-			onItemUpdate: callback
-		});
-    clientLs.subscribe(subscription);
-  },
-
-  getLatest(epic, direction) {
-    const marketData = {
-      'latest': '0',
-    };
-    const latestDirection = (direction === 'BUY') ? ['OFFER'] : ['BID'];
-    const clientLs = this.get('lsClient').getLsClient();
-    const market = [`MARKET:${epic}`];
-    var subscription = new Lightstreamer.Subscription(
-      "MERGE", market, latestDirection
-    );
-    subscription.setRequestedSnapshot("yes");
     subscription.addListener({
-      onSubscription: this.get('onSubscription'),
-      onUnsubscription: this.get('onUnsubscription'),
-      onSubscriptionError: this.get('onSubscriptionError'),
-      onItemUpdate: function(info) {
-        info.forEachField(function(fieldName, fieldPos, value) {
-          Ember.set(marketData, 'latest', value);
-        });
-      },
+      onItemUpdate: callback
     });
     clientLs.subscribe(subscription);
-    return marketData;
-  },
-  onSubscription() {
-    return console.log('pnl service subscription service started.');
-  },
-  onUnsubscription() {
-    return console.log('pnl service unsubscribe');
-  },
-  onSubscriptionError(code, message) {
-    return console.log(`pnl service error: ${message} with code: ${code}.`);
   },
   calculatePnl(direction, latest, openLevel, dealSize) {
     if (direction === 'BUY') {
