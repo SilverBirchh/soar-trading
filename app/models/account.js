1f98f87335e5b62e4e2f8c0e6909e2e0b4395518
Bradley Birch <Bradley.Birch@ig.com>
2016-07-08 11:16:34 +0100

    Balances refactor

--- app/models/account.js
+++ app/models/account.js
@@ -2,15 +2,14 @@ import DS from 'ember-data';
 import Ember from 'ember';
 
 export default DS.Model.extend({
-	balanceService: Ember.inject.service('balance-service'),
-
   accountType: DS.attr('string'),
 	preferred: DS.attr('boolean'),
 	status: DS.attr('string'),
 	accountName: DS.attr('string'),
 	currency: DS.attr('string'),
-
-	balance: Ember.computed(function() {
-		return this.get('balanceService').getBalances(this.get('id'));
-	})
+  PNL: null,
+  EQUITY: null,
+  FUNDS: null,
+  MARGIN: null,
+  AVAILABLE_TO_DEAL: null
 });
--- app/services/balance-service.js
+++ app/services/balance-service.js
@@ -4,15 +4,7 @@ export default Ember.Service.extend({
   lsClient: Ember.inject.service('ls-client'),
   session: Ember.inject.service('session'),
 
-  getBalances(accountId) {
-		const balances = {
-			'EQUITY': '0',
-			'PNL': '0',
-			'FUNDS': '0',
-			'MARGIN': '0',
-			'AVAILABLE_TO_DEAL': '0'
-		};
-
+  subscribe(accountId, callback) {
     const clientLs = this.get('lsClient').getLsClient();
     const fields = ['PNL', 'EQUITY', 'FUNDS', 'MARGIN', 'AVAILABLE_TO_DEAL'];
     const accountID = `ACCOUNT:${accountId}`;
@@ -21,25 +13,8 @@ export default Ember.Service.extend({
     );
     subscription.setRequestedSnapshot("yes");
     subscription.addListener({
-      onSubscription: this.get('onSubscription'),
-      onUnsubscription: this.get('onUnsubscription'),
-      onSubscriptionError: this.get('onSubscriptionError'),
-      onItemUpdate: function(info) {
-				info.forEachField(function(fieldName, fieldPos, value) {
-					Ember.set(balances, fieldName, value);
-				});
-			}
+      onItemUpdate: callback
     });
     clientLs.subscribe(subscription);
-		return balances;
   },
-  onSubscription() {
-    return console.log('Balance service subscription service started.');
-  },
-  onUnsubscription() {
-    return console.log('Balance service unsubscribe');
-  },
-  onSubscriptionError(code, message) {
-    return console.log(`Balance service error: ${message} with code: ${code}.`);
-  }
 });
